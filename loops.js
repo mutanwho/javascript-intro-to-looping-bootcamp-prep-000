f8a3e69853ab3df90447912a941a43b2a8320609
Michelle M <mutanwho@gmail.com>
2018-10-18 18:13:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ function doWhileLoop(array){
  do{
    array.pop();
    //incrementVariable();
- }while(array.lenght > 0 || incrementVariable());
+ }while(array.length > 0 || incrementVariable());
  
  return array;
 }
